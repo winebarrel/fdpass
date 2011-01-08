c8d1ab71865ee1eeeff759a3e29e38ed62526dfa
winebarrel <sgwr_dts@yahoo.co.jp>
2011-01-08 18:35:42 +0900

    remove NUM.close

--- ext/fdpass_server.c
+++ ext/fdpass_server.c
@@ -98,15 +98,6 @@ static VALUE rd_fdpass_server_initialize(VALUE self, VALUE path) {
   return Qnil;
 }
 
-static VALUE rd_fdpass_server_fd_close(VALUE fd) {
-  int ifd;
-
-  ifd = NUM2INT(fd);
-  close(ifd);
-
-  return Qnil;
-}
-
 static VALUE rb_fdpass_server_recv(VALUE self) {
   struct fdpass_socket *p;
   struct msghdr msg = { 0 };
@@ -115,7 +106,6 @@ static VALUE rb_fdpass_server_recv(VALUE self) {
   int *cmsg_data;
   struct iovec iov;
   char iov_data[1];
-  VALUE fd;
 
   Data_Get_Struct(self, struct fdpass_socket, p);
   Check_Socket(p);
@@ -137,10 +127,7 @@ static VALUE rb_fdpass_server_recv(VALUE self) {
   cmsg = CMSG_FIRSTHDR(&msg);
   cmsg_data = (int *) CMSG_DATA(cmsg);
 
-  fd = INT2NUM(*cmsg_data);
-  rb_define_singleton_method(fd, "close", rd_fdpass_server_fd_close, 0);
-
-  return fd;
+  return INT2NUM(*cmsg_data);
 }
 
 void Init_fdpass_server() {
